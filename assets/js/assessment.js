82a26606aba2e430e8d018397db0dadb9451a913
Dinesh Kumar <dinesh.kumar@kalpitatechnologies.com>
2023-02-17 18:25:20 +0530

    second commit

--- assets/js/assessment.js
+++ assets/js/assessment.js
@@ -1,15 +1,82 @@
 $(document).ready(() => {
   let productViewModel = {
+    productNumber: ko.observable(),
+    productDescription: ko.observable(),
+    weightPerUnitOfMeasure: ko.observable(),
+    activeProduct: ko.observable(),
+    listPrice: ko.observable(),
+
+    toastBackgroundColorClassName: ko.observable(),
+    toastMessage: ko.observable(),
+
     saveProductDetails: () => {
       let valid = validateBeforeSave();
       if (valid) {
-        console.log(valid);
+        let payload = preparePayload();
+        postProductDetails(payload);
+      } else {
+        showToastMessage(
+          PRODUCT_FORM_ERROR_MESSAGE,
+          CLASS_NAME_FOR_ERROR_TOAST
+        );
       }
     },
   };
-  ko.applyBindings(productViewModel);
-});
 
-function validateBeforeSave() {
-  return true;
-}
+  ko.applyBindings(productViewModel);
+
+  function validateBeforeSave() {
+    let valid = false;
+    if (
+      productViewModel.productNumber() &&
+      productViewModel.productDescription() &&
+      productViewModel.weightPerUnitOfMeasure() &&
+      productViewModel.activeProduct() &&
+      productViewModel.listPrice()
+    ) {
+      valid = true;
+    }
+    return valid;
+  }
+
+  function showToastMessage(message, bgColor) {
+    productViewModel.toastMessage(message);
+    productViewModel.toastBackgroundColorClassName(bgColor);
+    const toastForProduct = document.getElementById(
+      "toastForProductDetailsForm"
+    );
+    const toast = new bootstrap.Toast(toastForProduct);
+    toast.show();
+  }
+
+  function preparePayload() {
+    let payload = {
+      productNumber: productViewModel.productNumber(),
+      productDescription: productViewModel.productDescription(),
+      weightPerUnitOfMeasure: productViewModel.weightPerUnitOfMeasure(),
+      activeProduct: productViewModel.activeProduct(),
+      listPrice: productViewModel.listPrice(),
+    };
+    return payload;
+  }
+
+  function postProductDetails(payload) {
+    let jsonPayload = ko.toJSON(payload);
+    let promise = fetch(URL_LIST.PRODUCT_SAVE_URL, {
+      method: "post",
+      body: jsonPayload,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    promise
+      .then((e) => e.json())
+      .then((response) => {
+        showToastMessage(
+          PRODUCT_FORM_SAVED_SUCCESS_MESSAGE,
+          CLASS_NAME_FOR_SUCCESS_TOAST
+        );
+        console.log(response);
+      });
+  }
+});
---
+++ assets/js/global.js
@@ -0,0 +1,10 @@
+const URL_HEADER = "http://localhost:3000/";
+const URL_LIST = {
+  PRODUCT_SAVE_URL: `${URL_HEADER}products`,
+};
+
+const CLASS_NAME_FOR_ERROR_TOAST = "text-bg-danger";
+const CLASS_NAME_FOR_SUCCESS_TOAST = "text-bg-success";
+
+const PRODUCT_FORM_ERROR_MESSAGE = 'Please check the form once again and submit.';
+const PRODUCT_FORM_SAVED_SUCCESS_MESSAGE = 'Product details got saved successfully.';
